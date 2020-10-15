43dfb831e80ae35466fce95e3946bb714ded8217
JakeKViti <thevitij64@gmail.com>
2020-10-14 23:14:45 -0400

    sign up now works properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
 		  redirect to "/failure"
     else
       
-      user = User.new(:username => params[:username], :password_digest => params[:password])
+      user = User.create(:username => params[:username], :password => params[:password])
 		  redirect to "/login"
 		end
   end
