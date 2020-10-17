b7cc28ace12ec9e98c89713646e5cee621c89685
JakeKViti <thevitij64@gmail.com>
2020-10-17 01:02:44 -0400

    made it possible to add teams from an html form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,27 @@ class TeamsController < ApplicationController
     get "/index" do
         erb :'teams/index'
     end
+
+    get "/createteams" do
+        erb :'teams/createteams'
+      end
+
+    post "/createteams" do
+        if params[:sport] == "" || params[:num_of_players] == "" 
+          redirect to "/error"
+        else
+              
+        user = Teams.create(:sport => params[:sport], :num_of_players => params[:num_of_players], :coach_id => params[:coach_id])
+        redirect to "/success"
+        end
+    end
+
+    get "/error" do
+        erb :'teams/error'
+      end
+
+    get "/success" do
+        erb :'teams/success'
+    end
+
 end
